b44d7788ff5ffba4601635643f45aed6f4be30db
AbsoZed <Barker.21.b@gmail.com>
2019-05-17 00:25:12 +0000

    Create ProcMon.sh

---
+++ PrivEsc/Linux|Unix/ProcMon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Loop by line
+IFS=$'\n'
+
+old_process=$(ps aux --forest | grep -v "ps aux --forest" | grep -v "sleep 1" | grep -v $0)
+
+while true; do
+  new_process=$(ps aux --forest | grep -v "ps aux --forest" | grep -v "sleep 1" | grep -v $0)
+  diff <(echo "$old_process") <(echo "$new_process") | grep [\<\>]
+  sleep 1
+  old_process=$new_process
+done
